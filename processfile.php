6fd67f50c14c7ba4a54ce4bcae0d32c709ef7ce6
trevor davis <tdavis@ourfuture.org>
2014-04-14 12:01:52 -0400

    initial commit

---
+++ processfile.php
@@ -0,0 +1,178 @@
+<?php
+
+ini_set("auto_detect_line_endings", "1");
+
+$array = csv_to_array($_FILES["uploadFile"]["tmp_name"]);
+
+
+if (!is_array($array))
+{
+	echo "ERROR:  Not a readable file.";
+	exit;
+}
+
+$test = $array[0];
+
+foreach ($test as $k=>$v)
+{
+	if (validEmail($v))
+	{
+		$emailKey = $k;
+	}
+	else {
+		$uniqueID  = $k;
+		}
+
+}
+
+$newarray = array();
+
+foreach ($array as $row)
+{	
+	$email = strtolower(trim($row[$emailKey]));
+
+	$hashedemail=md5($email);
+
+
+	if (strlen($email)<1000)
+	{
+		$newarray[]=array($uniqueID =>$row[$uniqueID],'hashedemail'=>$hashedemail);
+	}
+}
+$data =  array_to_scv($newarray);
+
+    header("Content-type: application/octet-stream");
+	header("Content-Disposition: attachment; filename=\"md5email.csv\"");
+	echo $data;
+/**
+ * @link http://gist.github.com/385876
+ */
+function csv_to_array($filename='', $delimiter=',')
+{
+    if(!file_exists($filename) || !is_readable($filename))
+        return FALSE;
+
+    $header = NULL;
+    $data = array();
+    if (($handle = fopen($filename, 'r')) !== FALSE)
+    {
+        while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+        {
+            if(!$header)
+                $header = $row;
+            else
+                $data[] = array_combine($header, $row);
+        }
+        fclose($handle);
+    }
+    return $data;
+}
+
+
+/**
+Validate an email address.
+Provide email address (raw input)
+Returns true if the email address has the email 
+address format and the domain exists.
+*/
+function validEmail($email)
+{
+   $isValid = true;
+   $atIndex = strrpos($email, "@");
+   if (is_bool($atIndex) && !$atIndex)
+   {
+      $isValid = false;
+   }
+   else
+   {
+      $domain = substr($email, $atIndex+1);
+      $local = substr($email, 0, $atIndex);
+      $localLen = strlen($local);
+      $domainLen = strlen($domain);
+      if ($localLen < 1 || $localLen > 64)
+      {
+         // local part length exceeded
+         $isValid = false;
+      }
+      else if ($domainLen < 1 || $domainLen > 255)
+      {
+         // domain part length exceeded
+         $isValid = false;
+      }
+      else if ($local[0] == '.' || $local[$localLen-1] == '.')
+      {
+         // local part starts or ends with '.'
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $local))
+      {
+         // local part has two consecutive dots
+         $isValid = false;
+      }
+      else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain))
+      {
+         // character not valid in domain part
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $domain))
+      {
+         // domain part has two consecutive dots
+         $isValid = false;
+      }
+      else if
+(!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
+                 str_replace("\\\\","",$local)))
+      {
+         // character not valid in local part unless 
+         // local part is quoted
+         if (!preg_match('/^"(\\\\"|[^"])+"$/',
+             str_replace("\\\\","",$local)))
+         {
+            $isValid = false;
+         }
+      }
+      if ($isValid && !(checkdnsrr($domain,"MX") || 
+ ↪checkdnsrr($domain,"A")))
+      {
+         // domain not found in DNS
+         $isValid = false;
+      }
+   }
+   return $isValid;
+}
+
+
+/**
+* Generatting CSV formatted string from an array.
+* By Sergey Gurevich.
+*/
+function array_to_scv($array, $header_row = true, $col_sep = ",", $row_sep = "\n", $qut = '"')
+{
+	if (!is_array($array) or !is_array($array[0])) return false;
+	
+	//Header row.
+	if ($header_row)
+	{
+		foreach ($array[0] as $key => $val)
+		{
+			//Escaping quotes.
+			$key = str_replace($qut, "$qut$qut", $key);
+			$output .= "$col_sep$qut$key$qut";
+		}
+		$output = substr($output, 1)."\n";
+	}
+	//Data rows.
+	foreach ($array as $key => $val)
+	{
+		$tmp = '';
+		foreach ($val as $cell_key => $cell_val)
+		{
+			//Escaping quotes.
+			$cell_val = str_replace($qut, "$qut$qut", $cell_val);
+			$tmp .= "$col_sep$qut$cell_val$qut";
+		}
+		$output .= substr($tmp, 1).$row_sep;
+	}
+	
+	return $output;
+}
